785329dc808d4631dacff62ae5e8d1e1fe802a3f
Manvitha-486 <jsmr486@gmail.com>
2025-04-11 09:56:08 +0530

    Delete Back end directory

--- Back end/mongoAtlas/models/schema.js
+++
@@ -1,6 +0,0 @@
-const mongoose=require('mongoose');
-const userSchema=new mongoose.Schema({
-    name:{type:String,required:true},
-    price:{type:Number,required:true}
-})
-module.exports = new mongoose.model('User',userSchema)
