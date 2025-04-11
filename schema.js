dcd680e9d623a4ec3e90187c3d9644d2341203cd
Manvitha-486 <jsmr486@gmail.com>
2025-04-11 10:01:13 +0530

    Delete schema.js

--- schema.js
+++
@@ -1,6 +0,0 @@
-const mongoose=require('mongoose');
-const userSchema=new mongoose.Schema({
-    name:{type:String},
-    age:{type:Number}
-})
-module.exports = new mongoose.model('User',userSchema)
